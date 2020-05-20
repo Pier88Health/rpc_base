bdfdfed73c05b8286264e8b25cc1fd2ff3dc044a
dhaibing <renlanchuke@gmail.com>
2020-05-20 17:53:42 +0800

    server

---
+++ server.js
@@ -0,0 +1,42 @@
+/*jslint node:true*/
+const grpc = require('grpc'),
+    assert = require('assert'),
+    protoLoader = require('@grpc/proto-loader');
+;
+const defaultOptions = {
+    protoFolder: __dirname + '/proto/'
+}
+class RpcServer{
+    constructor(options = {}) {
+        assert(options.bindPoint, '[RpcServer] options.bindPoint is required')
+        this.options = Object.assign({}, defaultOptions, options);
+        this.server = new grpc.Server();
+    }
+
+    async addService(serviceName, methodMap) {
+        assert(serviceName, '[RpcServer.addService] params.serviceName is required');
+        assert(methodMap, '[RpcServer.addService] params.methodMap is required');
+        let protoPath = this.options.protoFolder + serviceName + '.proto',
+            packageDefinition,
+            proto;
+        packageDefinition = protoLoader.loadSync(
+            protoPath,
+            {
+                keepCase: true,
+                longs: String,
+                enums: String,
+                defaults: true,
+                oneofs: true
+            }
+        );
+        proto = grpc.loadPackageDefinition(packageDefinition)[serviceName];
+        this.server.addService(proto[serviceName].service, methodMap);
+    }
+
+    start() {
+        this.server.bind(this.options.bindPoint, grpc.ServerCredentials.createInsecure());
+        this.server.start();
+    }
+}
+
+module.exports = RpcServer;
