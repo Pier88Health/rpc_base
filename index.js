2c7c81d214a8521fa2c9c0931bde19f785b6d02e
dhaibing <renlanchuke@gmail.com>
2020-05-20 17:53:52 +0800

    index

---
+++ index.js
@@ -0,0 +1,6 @@
+/*jslint node:true*/
+
+module.exports.RpcClient = require('./client.js');
+module.exports.RpcServer = require('./server.js');
+module.exports.ConfigRegistry = require('./registry/confgRegistry.js');
+module.exports.RedisRegistry = require('./registry/redisRegistry.js');
