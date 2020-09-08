a782e815678d97414d0fd95d030c1c3a39ad56f6
dhaibing <renlanchuke@gmail.com>
2020-09-08 19:37:19 +0800

    config registry no register

--- registry/confgRegistry.js
+++ registry/confgRegistry.js
@@ -7,22 +7,21 @@ class ConfigRegistry extends RegistryBase {
         assert(options.address, '[ConfigRegistry] options.address is required');
         super();
         this.options = options;
-        this._subscribeMap = new Map(); // <interfaceName, addressList>
-        this._registerMap = new Map();
+        this.subscribeMap = new Map(); // <interfaceName, addressList>
+        this.registerMap = new Map();
         this._init();
     }
 
     _init() {
         assert(fs.existsSync(this.options.address), `[ConfigRegistry] options.address: ${this.options.address} file not exists`);
         const configData = fs.readFileSync(this.options.address);
-        this._subscribeMap = JSON.parse(configData);
-        this._ready = true;
+        this.subscribeMap = JSON.parse(configData);
     }
 
     _subscribe(config, listener) {
         assert(config && config.serviceName, '[ConfigRegistry] subscribe(config, listener) config.ServiceName is required');
         const serviceName = config.serviceName,
-            address = this._subscribeMap[serviceName];
+            address = this.subscribeMap[serviceName];
         if (address && address.length) {
             listener(address);
         }
@@ -40,22 +39,14 @@ class ConfigRegistry extends RegistryBase {
             this.removeAllListeners(serviceName);
         }
         if (this.listenerCount(serviceName) === 0) {
-            this._subscribeMap.delete(serviceName);
+            this.subscribeMap.delete(serviceName);
         }
     }
 
-    _register(config) {
-        assert(config && config.serviceName, '[ConfigRegistry] register(config) config.serviceName is required');
-        assert(config.host, '[ConfigRegistry] register(config) config.host is required');
-        if (!this._registerMap.has(config.serviceName)) {
-            this._registerMap.set(config.serviceName, [config.host]);
-        }
+    _register() {
     }
 
-    _unregister(config) {
-        assert(config && config.serviceName, '[ConfigRegistry] register(config) config.serviceName is required');
-        assert(config.host, '[configRegistry] register(config) config.host is required');
-        this._registerMap.delete(config.serviceName);
+    _unregister() {
     }
 }
 
